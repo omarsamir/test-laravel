29ebf60f4467479988db9739451f0ec048405b7e
Omar <omarsamir@live.co.uk>
2016-01-06 23:25:55 +0200

    database configrations and migration (articles and tags)

---
+++ laravel-blog/blog/database/migrations/2016_01_06_211549_CreateTagsTable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    	Schema::create('tags',function (Blueprint $table){
+    		$table->increments('id');
+    		$table->string("tagName");
+    		$table->date("creationDate");
+    		$table->integer("resourcesCount");
+    	});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    	Schema::drop('tags');
+    }
+}
---
+++ laravel-blog/blog/database/migrations/2016_01_06_211757_CreateArticleTable.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    	Schema::create('articles',function (Blueprint $table){
+    		$table->increments('id');
+    		$table->string("articleTitle");
+    		$table->string("articleSubTite");
+    		$table->date("publishDate");
+    		$table->date("modifiedDate");
+    		$table->string("articleBody");
+    		$table->string("articleSummry");
+    	});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    	Schema::drop('articles');
+    }
+}
