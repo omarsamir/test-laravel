f330f60569fc6f086479673e86ae83ad6c5d0cd1
Omar <omarsamir@live.co.uk>
2016-01-06 19:15:15 +0200

    fiest push test

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <div class="title"> pushtest</div>
             </div>
         </div>
     </body>
